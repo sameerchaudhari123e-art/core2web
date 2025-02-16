41366909cd3627f32842e525fce64f7c9be0365c
sameerchaudhari123e-art <sameerchaudhari542@gmail.com>
2025-02-16 15:33:52 +0530

    Add files via upload

---
+++ c2w java operator/q2.java
@@ -0,0 +1,12 @@
+class relationalpro{
+public static void main(String[] args){
+       int x= 7;
+       int y= 5;
+System.out.println(x<y);
+System.out.println(x>y);
+System.out.println(x<=y);
+System.out.println(x>=y);
+System.out.println(x!=y);
+}
+}
+
---
+++ c2w java operator/q6.java
@@ -0,0 +1,7 @@
+class increamentop{
+public static void main(String[] args){
+ int x= 19;
+System.out.println(x++ + x++);
+System.out.println(++x +x++ + ++x); 
+}
+}
